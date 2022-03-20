504069ac9986ffb3b848ca8d1c9fc7c158add050
爱走位的KN_sword <83012768+ShrLeeKNsword@users.noreply.github.com>
2022-03-21 07:20:34 +0800

    Update 41workroom_check_human.js

--- js/41workroom_check_human.js
+++ js/41workroom_check_human.js
@@ -57,7 +57,7 @@ function GetRandomNum(Min,Max)
 
 
 window.onload = function () {
-    var url = "HTTPS://drbd.rpgca.tk"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
+    var url = "HTTPS://drbd.rpgca.tk/checkqs.json"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
     var request = new XMLHttpRequest();
     request.open("get", url);/*设置请求方法与路径*/
     request.send(null);/*不发送数据到服务器*/
